4cc922e2b54c49f00577684597ad034292b69a66
ishita <chauhanishita5356@gmail.com>
2023-06-20 10:36:03 +0530

    Task 1911/ishita_chauhan/added python variables and keywords

---
+++ modulo_string_input.py
@@ -0,0 +1,24 @@
+################# modulus operator
+remainder = 7 % 3
+print(remainder)
+
+################# string operations
+first = '100'
+second = '150'
+print(first + second)
+
+first = 'Test '
+second = 3
+print(first * second)
+
+################# take input
+inp = input()
+#type something
+
+name = input('What is your name?\n')
+#type something
+
+print(inp,name)
+
+#to get data as integer
+speed = input()
---
+++ values_variables_keywords.py
@@ -0,0 +1,25 @@
+# values eg
+print(1,000,000)
+
+
+################# variables eg
+x = 5 #int
+y = "John" #string
+print(x)
+print(y)
+print(type(x))
+print(type(y))
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+################# operators and operands
+hour = 2
+minute = 5
+print(20+32)
+print(hour-1)
+print(hour*60+minute)
+print(minute/60)
+print(5**2)
+print((5+9)*(15-7))
+print(59/60) #floating point in python 3.x
