67179c3f5ed86d46d2ed51083903a2d3fce8310a
ishita <chauhanishita5356@gmail.com>
2023-06-20 11:27:54 +0530

    Task 1911/ishita_chauhan/added operators

---
+++ operators.py
@@ -0,0 +1,23 @@
+####### Boolean expressions
+print(5 == 5)
+print(5 == 6)
+print(type(True))
+print(type(False))
+
+####### comparison operators
+x = 8
+y = 10
+print(x != y)           # x is not equal to y
+print(x > y)           # x is greater than y
+print(x < y)           # x is less than y
+print(x >= y)           # x is greater than or equal to y
+print(x <= y)           # x is less than or equal to y
+print(x is y)           # x is the same as y
+print(x is not y)           # x is not the same as y
+
+####### Logical operators
+n = 9
+print(x > 0 and x < 10)
+print(n%2 == 0 or n%3 == 0)
+print(not (x > y))
+print(17 and True)
